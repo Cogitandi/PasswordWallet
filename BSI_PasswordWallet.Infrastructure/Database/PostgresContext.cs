0dfa27e7de4779869c4a0bd2e652f9155c38f4e1
Pawel <p.kapica6@gmail.com>
2021-10-21 01:42:27 +0200

    add-database-logging

--- BSI_PasswordWallet.Infrastructure/Database/PostgresContext.cs
+++ BSI_PasswordWallet.Infrastructure/Database/PostgresContext.cs
@@ -1,5 +1,7 @@
 ﻿using BSI_PasswordWallet.Core.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +12,22 @@ namespace BSI_PasswordWallet.Infrastructure.Database
 {
     public class PostgresContext : DbContext
     {
+        private static ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+            .AddFile("app.log", append: true)
+            .AddConsole((options) => { })
+            .AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name
+                && level == LogLevel.Information);
+        });
         public DbSet<User> Users { get; set; }
         public DbSet<Password> Passwords { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
             optionsBuilder.UseNpgsql("Server=localhost;Database=BSI_PasswordWallet;User Id=user;Password=password;Port=1101");
+            optionsBuilder.EnableSensitiveDataLogging();
             base.OnConfiguring(optionsBuilder);
         }
     }
